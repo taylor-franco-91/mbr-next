33e29cfb30ad79a4b6632f84bfcad5abd83490da
Taylor Franco <taylor.franco91@gmail.com>
2025-09-25 19:32:46 -0500

    lint: disable react/no-unescaped-entities for JSX copy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/** @type {import('eslint').Linter.FlatConfig[]} */
+import next from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+export default [
+  next,
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    // We write marketing copy with quotes/apostrophes in JSX text,
+    // so disable this opinionated rule.
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
   },
 ];
-
-export default eslintConfig;
